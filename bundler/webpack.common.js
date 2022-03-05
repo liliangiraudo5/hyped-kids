a7e2e80408a2a08f8ebb8d9acb0cb74aaafaad4e
Lilian Giraudo <lilian.giraudo@gmail.com>
2022-03-05 16:04:44 +0100

    scss set up v2

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -63,7 +63,7 @@ module.exports = {
                 test: /\.scss$/,
                 use:
                 [
-                    'style-loader',
+                    MiniCSSExtractPlugin.loader,
                     'css-loader',
                     'sass-loader'
                 ]
