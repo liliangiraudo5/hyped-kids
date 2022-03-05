9ddccff878a11cdfe9662e1f9aa647e9dc5b267c
Lilian Giraudo <lilian.giraudo@gmail.com>
2022-03-05 18:53:00 +0100

    scss import other files

--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
 import './style/main.scss'
-import './style/icon.scss'
-import './style/home.scss'
-import './style/footer.scss'
 //import * as THREE from 'three'
 //import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
